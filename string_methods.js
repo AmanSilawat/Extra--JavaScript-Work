76893716851dab57647c6ca5572d59ac3b01fa6a
aman <aman.khan.silawat.aks@gmail.com>
2020-08-20 16:35:08 +0530

    new tast

--- string_methods.js
+++ string_methods.js
@@ -49,18 +49,138 @@ str.charAt(50) // " " - out of range return empty string
 */
 
 
-/*
 
-link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
-Heading: Getting whole characters
-use in programing:non-BMP character
-
-*/
 
 
 
 /* 7. charCodeAt ======================
 
+- the charCodeAt() method returns an integer
+- between 0 to 65,535 representing the UTF-16 code unit at the given index
+- The first 128 Unicode code points are a direct match of the ASCII character encoding. 
+- charCodeAt() returns NaN
+	=> if the given index is less than 0
+	=> if the index is greater than string length
+- 
 
+*/
+
+var str = "Hello this a string and i'm find something."
+
+str.charCodeAt(0) //72  index is not a number
+str.charCodeAt('aa') //72 (index not a number set default index to 0)
+str.charCodeAt(-5) //NaN (less than 0)
+str.charCodeAt(100) //NaN (less than 0)
+
+
+// SYNTEX: str.charCodeAt(index)
+
+/*PARAMETER : 
+	=> index:
+	- an integer greater than or equal to 0 and less than the length of the string.
+	- if index is not a number it default to 0
+*/
+
+
+
+
+
+
+/* 8. codePointAt =====================
+/*
+
+The codePointAt() method returns a non-negative integer
+that is the unicode code point value.
+
+*/
+
+const icons = '☃★♲';
+
+icons.codePointAt(1); // chrome:732, firefox: 9733
+icons.codePointAt(10); // undefined
+icons.codePointAt(-10); // undefined
+icons.codePointAt(0); // 226
+icons.codePointAt('asdf'); 226// (index not a number set default index to 0)
+
+// ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲
+// | | | | | | | | | |
+// different between
+// | | | | | | | | | |
+// ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼
+
+icons.charCodeAt(1); // chrome:732, firefox: 9733
+icons.charCodeAt(10); // NaN
+icons.charCodeAt(-10); // NaN
+icons.charCodeAt(0); // 226
+icons.charCodeAt('asdf'); 226// (index not a number set 
+
+
+// SYNTEX: str.codePointAt(pos)
+
+/*PARAMETER : 
+	=> index:
+	- an integer greater than or equal to 0 and less than the length of the string.
+	- if index is not a number it default to 0
+*/
+
+
+
+
+
+
+/* 9. concat =====================
+/*
+
+The concat() method concatenates the string arguments to the calling string and returns a new string.
+
+*/
+
+const str1 = 'Hello';
+const str2 = 'World';
+
+console.log(str1.concat(' ', str2)); //"Hello friend"
+console.log(str2.concat(', ', str1)); // "friend Hello"
+console.log(str2.concat(', ', [str1, 'zzz'])); // "friend Hello"
+
+
+
+
+// SYNTEX: str1.concat(str2, [, ..strN])
+
+/*PARAMETER : 
+	=> index:
+	- an integer greater than or equal to 0 and less than the length of the string.
+	- if index is not a number it default to 0
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+1.
+link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
+Heading: Getting whole characters
+question:non-BMP character
+
+2.
+non-Basic-Multilingual-Plane characters
+
+3.
+link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/codePointAt
+question : different browser different output
 
 */
