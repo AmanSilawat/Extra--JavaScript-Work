3172f9bfca4298bb9917fe81ac25b4f543a4b49d
aman <aman.khan.silawat.aks@gmail.com>
2020-09-18 19:03:17 +0530

    added more question

--- js_code.js
+++ js_code.js
@@ -7,8 +7,17 @@
     console.log('d');
 })();
 
-output:
-'a'
-'d'
-'c'
-'b'
+// output:
+// 'a'
+// 'd'
+// 'c'
+// 'b'
+
+
+// Q. What will this code print?
+for (let i = 0; i < 5; i++) {
+    setTimeout(function() { console.log(i); }, i * 1000 );
+}
+
+// output:
+// It will print 0 1 2 3 4, because we use let instead of var here. The variable i is only seen in the for loop’s block scope.
